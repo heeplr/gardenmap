00d413b3b37925d735bd83a0d7e28e1a7fd4c1c8
daniel <d@upquark>
2025-07-28 21:47:49 +0200

    use / for static files

--- gardenmap.py
+++ gardenmap.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, jsonify
 import json
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 DATA_FILE = 'plants.json'
 if not os.path.exists(DATA_FILE):
