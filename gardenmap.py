923f3b4c94722e42fe3091d2b38d4ff5b0f0bcd5
daniel <d@upquark>
2025-07-28 22:46:28 +0200

    add preliminary backend for serverside plant palette

--- gardenmap.py
+++ gardenmap.py
@@ -6,6 +6,7 @@ import os
 
 app = Flask(__name__, static_url_path='')
 
+PALETTE_FILE = 'palette.json'
 DATA_FILE = 'plants.json'
 if not os.path.exists(DATA_FILE):
     with open(DATA_FILE, 'w') as f:
@@ -14,6 +15,38 @@ if not os.path.exists(DATA_FILE):
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/palette', methods=['GET', 'POST', 'PUT'])
+def palette():
+    match request.method:
+
+        case 'POST':
+            new_plant = request.json
+            with open(PALETTE_FILE, 'r+') as f:
+                data = json.load(f)
+                data["palette"] += [ new_plant ]
+                f.seek(0)
+                json.dump(data, f, indent=2)
+                f.truncate()
+            return jsonify({'status': 'success'})
+
+        case 'PUT':
+            updated_plant = request.json
+            with open(PALETTE_FILE, 'r+') as f:
+                data = json.load(f)
+                for i, plant in enumerate(data["palette"]):
+                    if plant['id'] == updated_plant['id']:
+                        data["palette"][i] = updated_plant
+                        break
+                f.seek(0)
+                json.dump(data, f, indent=2)
+                f.truncate()
+            return jsonify({'status': 'updated'})
+
+        case _:
+            with open(PALETTE_FILE) as f:
+                data = json.load(f)
+            return jsonify(data)
 
 @app.route('/plants', methods=['GET', 'POST', 'PUT'])
 def plants():
